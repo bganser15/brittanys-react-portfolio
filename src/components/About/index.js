bee0b80674a67c8c4e9f36aa9172b2a35bbc13bd
Brittany Ganser <15brganser@gmail.com>
2022-06-26 22:43:15 -0600

    added headshot to about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./index.css"
 
-
 function About() {
   return (
     <section className="container">
       <div className="pic-container">
-        image goes here
+        <img src={require("../../assets/img/BrittanyHeadshot.jpg")} alt="Headshot of Brittany"></img>
       </div>
-      <div className="about-me-coontainer">
+      <div className="about-me-container">
         <h1>Hi! I'm Brittany</h1>
         <p>I graduated from Baldwin Wallace University with a BA in Theatre and minor in Web Programming. I am currently taking Michigan State University's Coding Bootcamp to become a full stack web developer. I am excited to take my love for art and design and translate it into beautiful websites.</p>
       </div>
