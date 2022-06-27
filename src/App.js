0677603cbc15b6c0f9290b91ea525b1126043c18
Brittany Ganser <15brganser@gmail.com>
2022-06-27 00:44:39 -0600

    added hooks for page change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useState } from "react";
 import About from "./components/About";
 import Nav from "./components/Nav";
 import Portfolio from "./components/Portfolio";
@@ -6,19 +7,50 @@ import Resume from "./components/Resume";
 import Contact from "./components/Contact";
 
 function App() {
+  const [aboutSelected, setAboutSelected] = useState(false);
+  const [portfolioSelected, setPortfolioSelected] = useState(false);
+  const [resumeSelected, setResumeSelected] = useState(false);
+  const [contactSelected, setContactSelected] = useState(false);
   return (
     <main>
-      <Nav></Nav>
-      <div>
-        <About></About>;
-      </div>
-      <div>
-        <Portfolio></Portfolio>
-      </div>
-      <div>
-        <Resume></Resume>
-      </div>
-      <Contact></Contact>
+      <Nav
+        aboutSelected={aboutSelected}
+        setAboutSelected={setAboutSelected}
+        portfolioSelected={portfolioSelected}
+        setPortfolioSelected={setPortfolioSelected}
+        resumeSelected={resumeSelected}
+        setResumeSelected={setResumeSelected}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      {portfolioSelected ? (
+        <>
+          <Portfolio></Portfolio>
+        </>
+      ) : (
+        <>
+          <About></About>
+        </>
+      )}
+      {resumeSelected ? (
+        <>
+          <Resume></Resume>
+        </>
+      ) : (
+        <>
+          <About></About>
+        </>
+      )}
+
+      {contactSelected ? (
+        <>
+          <Contact></Contact>
+        </>
+      ) : (
+        <>
+          <About></About>
+        </>
+      )}
     </main>
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,23 +1,65 @@
 import React from "react";
-import "./index.css"
+import "./index.css";
 
-
-function Nav() {
+function Nav(props) {
+  const {
+    setAboutSelected,
+    setPortfolioSelected,
+    setResumeSelected,
+    setContactSelected,
+  } = props;
   return (
     <header>
       <nav>
         <ul>
           <li>
-            <a href="#about">About</a>
+            <a
+              href="#about"
+              onClick={() => {
+                setAboutSelected(true);
+                setPortfolioSelected(false);
+                setResumeSelected(false)
+                setContactSelected(false);
+              }}
+            >
+              About
+            </a>
           </li>
           <li>
-            <a href="#portfolio">Portfolio</a>
+            <a
+              href="#portfolio"
+              onClick={() => {
+                setPortfolioSelected(true);
+                setAboutSelected(false);
+                setResumeSelected(false)
+                setContactSelected(false);
+              }}
+            >
+              Portfolio
+            </a>
           </li>
           <li>
-            <a href="#resume">Resume</a>
+            <a href="#resume" onClick={() => {
+              setResumeSelected(true)
+              setAboutSelected(false)
+              setPortfolioSelected(false);
+              setContactSelected(false);
+              }}>
+              Resume
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a
+              href="#contact"
+              onClick={() => {
+                setContactSelected(true);
+                setAboutSelected(false);
+                setPortfolioSelected(false);
+                setResumeSelected(false)
+              }}
+            >
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
