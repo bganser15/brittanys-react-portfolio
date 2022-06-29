2f8b2db18af96b06a1712c302c4bbfe30b730fce
Brittany Ganser <15brganser@gmail.com>
2022-06-29 00:04:39 -0600

    updated various styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./index.css"
 
+
+
 function Footer() {
     return (
 <section className="getInTouchSection">
@@ -8,26 +10,25 @@ function Footer() {
                 <div className="icons">
                     <div className="links">
                         <a href="https://github.com/bganser15" target="_blank" rel="noreferrer">
-                        <img src="./assets/images/github.svg" href="https://bganser15.github.io/run-buddy/"alt=""/>
+                        <img src={require("../../assets/img/github.svg").default} href="https://bganser15.github.io/run-buddy/"alt=""/>
                         <h3>Github</h3></a>
                     </div>
 
                 
                     <div className="links">
                         <a href="mailto:15brganser@gmail.com">
-                        <img src="./assets/images/email.svg" alt=""/>
+                        <img src={require("../../assets/img/email.svg").default} alt=""/>
                         <h3>Email</h3></a>
                     </div>
                     
                     
                     <div className="links">
                         <a href="https://www.linkedin.com/in/brittanyganser/" target="_blank" rel="noreferrer">
-                        <img src="./assets/images/linked-in.svg" alt=""/>
+                        <img src={require("../../assets/img/linkedin.png")} alt=""/>
                         <h3>LinkedIn</h3></a>
                     </div>
                 </div>
         </section> 
     )
     }
-
-    export default Footer;
+export default Footer;
