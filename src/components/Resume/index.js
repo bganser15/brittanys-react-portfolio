63bac2b32d72671d05e26f640152b77f5842f1ee
Brittany Ganser <15brganser@gmail.com>
2022-06-30 20:15:18 -0600

    fixed resume download

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,22 +1,26 @@
 import React from "react";
-import "./index.css"
+import "./index.css";
 function Resume() {
   return (
     <section className="resume-container">
       <div className="skills">
-      <h1>Skills</h1>
-      <ul>
-        <li>HTML and CSS</li>
-        <li>Javascript(ES6)</li>
-        <li>Node.js</li>
-        <li>Express.js</li>
-        <li>MySQL</li>
-        <li>MongoDB</li>
-        <li>React</li>
-        <li>Github</li>
-      </ul>
+        <h1>Skills</h1>
+        <ul>
+          <li>HTML and CSS</li>
+          <li>Javascript(ES6)</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>MySQL</li>
+          <li>MongoDB</li>
+          <li>React</li>
+          <li>Github</li>
+        </ul>
       </div>
-      <button className="resume-button"><a href="../../assets/docs/BrittanyGanserResume.pdf" download>Download My Resume</a></button>
+      <button className="resume-button">
+        <a href="/BrittanyGanserResume.pdf" download>
+          Download My Resume
+        </a>
+      </button>
     </section>
   );
 }
